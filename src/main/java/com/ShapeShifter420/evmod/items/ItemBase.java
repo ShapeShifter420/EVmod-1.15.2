78cd5ee4b43aaa0ec0d8e9404fa73c7c73c9c2b9
ShapeShifter420 <nikita24.10nm@yandex.ru>
2020-04-17 05:06:33 +0500

    основа объектов(вылетает)

---
+++ src/main/java/com/ShapeShifter420/evmod/items/ItemBase.java
@@ -0,0 +1,12 @@
+package com.ShapeShifter420.evmod.items;
+
+import com.ShapeShifter420.evmod.evmod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class ItemBase extends Item {
+
+    public ItemBase() {
+        super(new Item.Properties().group(ItemGroup.MATERIALS));
+    }
+}
---
+++ src/main/java/com/ShapeShifter420/evmod/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package com.ShapeShifter420.evmod.util;
+
+import com.ShapeShifter420.evmod.evmod;
+import com.ShapeShifter420.evmod.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, evmod.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // Items
+    public static final RegistryObject<Item> CPU = ITEMS.register("cpu", ItemBase::new);
+
+}
